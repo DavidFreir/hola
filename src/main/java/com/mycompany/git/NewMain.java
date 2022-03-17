a7599eda987a833614bc2c65d8f81347c6287bba
david.freirepereira <david.freirepereira@DOC32B.wirtz.local>
2022-03-17 18:31:07 +0100

    tercera version rama

--- src/main/java/com/mycompany/git/NewMain.java
+++ src/main/java/com/mycompany/git/NewMain.java
@@ -3,6 +3,6 @@ package com.mycompany.git;
 public class NewMain {
 
     public static void main(String[] args) {
-        System.out.println(Sumador.sumar(15f, 4f));
+        System.out.println(Restador.restar(15f, 4f));
     }
 }
--- src/main/java/com/mycompany/git/Sumador.java
+++
@@ -1,8 +0,0 @@
-package com.mycompany.git;
-
-public class Sumador {
-
-    public static float sumar(float a, float b) {
-        return a + b;
-    }
-}
